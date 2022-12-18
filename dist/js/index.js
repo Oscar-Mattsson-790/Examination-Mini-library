b1f70fcc22ee230c57e1660b6327c84dc49674a9
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2022-12-18 23:19:59 +0100

    Styling done

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,27 @@
-console.log("This is a map");
+"use strict";
+
+async function getBooks() {
+  try {
+    const response = await fetch(
+      "https://my-json-server.typicode.com/zocom-christoffer-wallenberg/books-api/books"
+    );
+    console.log(response);
+    if (response.status === 200) {
+      const data = await response.json();
+      console.log(data);
+    } else {
+      throw Error("Något gick fel, försök igen senare");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+getBooks();
+
+const mainElem = document.querySelector("main");
+const bookElem = document.querySelector(".book1");
+
+bookElem.addEventListener("click", () => {
+  console.log("Clicked");
+  mainElem.style.display = "none";
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("This is a map");
+async function getBooks() {
+  try {
+    const response = await fetch(
+      "https://my-json-server.typicode.com/zocom-christoffer-wallenberg/books-api/books"
+    );
+    console.log(response);
+    if (response.status === 200) {
+      const data = await response.json();
+      console.log(data);
+    } else {
+      throw Error("Något gick fel, försök igen senare");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+getBooks();
